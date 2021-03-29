930cabfabef0d9cffa0391c0be78800b61b6f3ca
Mateusz <s19282@pjwstk.edu.pl>
2021-03-29 14:22:20 +0200

    z4 not finished

---
+++ app/src/com/company/Engine.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Engine
+{
+    public void makeSound(EngineType engineType)
+    {
+        System.out.println("Rotary sound");
+    }
+    public void makeSound(EngineType engineType)
+    {
+        System.out.println("Rotary sound");
+    }
+}
---
+++ app/src/com/company/EngineType.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public enum EngineType
+{
+    ROTARY, PISTON, ELECTRIC
+}
