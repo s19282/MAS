7732dc16fef7d6b3c20197f5c31193f258924d98
Mateusz <s19282@pjwstk.edu.pl>
2021-03-29 13:12:00 +0200

    z1

--- app/src/com/company/Main.java
+++ app/src/com/company/Main.java
@@ -1,9 +1,24 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        z1(8,12);
     }
+
+    static void z1(int a, int b)
+    {
+        int[] tab = new int[10];
+        for(int i=0; i<tab.length; i++)
+        {
+            tab[i]=(int)(Math.random()*(b-a+1)+a);
+            System.out.println(tab[i]);
+        }
+        System.out.println("Max generated value: "+Arrays.stream(tab).max().getAsInt());
+        System.out.println("Min generated value: "+Arrays.stream(tab).min().getAsInt());
+    }
+
 }
