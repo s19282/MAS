3c3ae7c65874cfb17482a86451ef20f93dc8ee75
Mateusz <s19282@pjwstk.edu.pl>
2021-03-29 13:41:58 +0200

    z2

--- app/src/com/company/Main.java
+++ app/src/com/company/Main.java
@@ -1,12 +1,14 @@
 package com.company;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main
 {
     public static void main(String[] args)
     {
         z1(8,12);
+        z2();
     }
 
     static void z1(int a, int b)
@@ -19,6 +21,56 @@ public class Main
         }
         System.out.println("Max generated value: "+Arrays.stream(tab).max().getAsInt());
         System.out.println("Min generated value: "+Arrays.stream(tab).min().getAsInt());
+    }
+
+    static void z2()
+    {
+        int[] array = new int[5];
+        List<Integer> list = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        Map<Integer,String> map = new HashMap<>();
+
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(5);
+
+        map.put(1,"1");
+        map.put(2,"2");
+        map.put(3,"3");
+        map.put(4,"4");
+        map.put(5,"5");
+
+        for (int i : array)
+        {
+            array[i-1] = i>3 ? -1 : i;
+        }
+
+        list = list.stream().filter(i->i<3).collect(Collectors.toList());
+        list.removeAll(list.stream().filter(i->i>2).collect(Collectors.toList()));
+
+        set.removeAll(set.stream().filter(i->i>3).collect(Collectors.toSet()));
+
+        map.remove(1);
+        map.entrySet().removeAll(map.entrySet().stream().filter(v->v.getKey()>3).collect(Collectors.toList()));
+
+        System.out.println(Arrays.toString(array));
+        System.out.println(list);
+        System.out.println(set);
+        System.out.println(map);
     }
 
 }
