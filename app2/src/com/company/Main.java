7ccb82b3e35f900145e1e94ca0ae0cd18fa1c426
Mateusz <s19282@pjwstk.edu.pl>
2021-03-31 23:10:22 +0200

    z4

--- app2/src/com/company/Main.java
+++ app2/src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -10,6 +17,7 @@ public class Main {
 	    z1();
 	    z2();
 	    z3();
+	    z4(Integer.parseInt(args[0]));
     }
 
     static void z1()
@@ -29,6 +37,7 @@ public class Main {
             list.add(random.nextInt(100));
         list.stream().filter(i->i%2==1).forEach(System.out::println);
     }
+
     static void z3()
     {
         List<Integer> list = new ArrayList<>();
@@ -37,5 +46,17 @@ public class Main {
             list.add(random.nextInt(100));
         System.out.println("Sum: "+list.stream().mapToInt(Integer::intValue).sum());
 
+    }
+
+    static void z4(int number)
+    {
+        try (Stream<String> stream = Files.lines(Paths.get("file.txt")))
+        {
+            stream.limit(number).forEach(System.out::println);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
