cf572666ec7a02f392b3b55caaa6feb1c45841b4
Mateusz <s19282@pjwstk.edu.pl>
2021-03-31 22:58:03 +0200

    z3

--- app2/src/com/company/Main.java
+++ app2/src/com/company/Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
 	    z1();
+	    z2();
+	    z3();
     }
 
     static void z1()
@@ -13,5 +19,23 @@ public class Main {
 
         MyMap<Double,Float> map2 = new MyMap<>(1D,1F);
         System.out.println(map2);
+    }
+
+    static void z2()
+    {
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for(int i=0; i<100; i++)
+            list.add(random.nextInt(100));
+        list.stream().filter(i->i%2==1).forEach(System.out::println);
+    }
+    static void z3()
+    {
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for(int i=0; i<100; i++)
+            list.add(random.nextInt(100));
+        System.out.println("Sum: "+list.stream().mapToInt(Integer::intValue).sum());
+
     }
 }
