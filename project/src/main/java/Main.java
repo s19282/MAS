f4d23f5456140c23d828dad12258c51fa769a65e
Mateusz <s19282@pjwstk.edu.pl>
2021-06-18 19:00:43 +0200

    created simpler method, JComboBox finished

--- project/src/main/java/Main.java
+++ project/src/main/java/Main.java
@@ -10,8 +10,10 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Main
@@ -22,9 +24,253 @@ public class Main
         frame.setSize(800,600);
         frame.setResizable(false);
 
-        listaNapraw(frame);
+        listaNaprawWersja2(frame);
 
         frame.setVisible(true);
+    }
+
+    public static void listaNaprawWersja2(JFrame frame)
+    {
+        List<Osoba> pracownicyZBazy = pobierzPracownikowZBazy();
+        Set<Naprawa> unikalneNaprawy = new HashSet<>();
+        JLabel naglowekLabel = new JLabel("Lista napraw");
+
+        JLabel pracownicyLabel = new JLabel("Pracownicy: ");
+        JComboBox<String> pracownicy = new JComboBox<>(new String[]{"Wszyscy pracownicy"});
+        pracownicyZBazy.forEach(p -> pracownicy.addItem(p.toString()));
+
+        JLabel naprawyLabel = new JLabel("Naprawy: ");
+        JComboBox<String> naprawy = new JComboBox<>(new String[]{"Wszystkie naprawy"});
+        pracownicyZBazy.forEach(pracownik -> {
+            try {
+                unikalneNaprawy.addAll(pracownik.getNaprawy());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        unikalneNaprawy.forEach(n -> naprawy.addItem(n.toString()));
+
+//        JComboBox<Naprawa> naprawy = new JComboBox<>(naprawyZBazy.toArray(Naprawa[]::new));
+        MyCellRenderer cellRenderer = new MyCellRenderer(600);
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+//        naprawyZBazy.forEach(n->listModel.addElement(n.toString()));
+        JList<String> lista = new JList<>(listModel);
+        lista.setCellRenderer(cellRenderer);
+
+        MyHelper helper = new MyHelper();
+        pracownicy.addActionListener(e -> {
+            if(!helper.isRunning()) // Zabezpieczenie przeciwko zapętleniu
+            {
+                helper.setRunning(true);
+
+                if(pracownicy.getSelectedIndex() != 0) // Wybrany jakiś pracownik
+                {
+                    if(naprawy.getSelectedIndex() != 0) // Wybrana jakaś naprawa
+                    {
+                        Object selected = naprawy.getSelectedItem();
+                        naprawy.removeAllItems();
+                        naprawy.addItem("Wszystkie naprawy");
+                        try {
+                            pracownicyZBazy.get(pracownicy.getSelectedIndex()-1).getNaprawy().forEach(n -> naprawy.addItem(n.toString()));
+                        } catch (Exception exception) {
+                            exception.printStackTrace();
+                        }
+                        naprawy.setSelectedItem(selected);
+                    }
+                    else // Wybrane wszystkie naprawy
+                    {
+                        naprawy.removeAllItems();
+                        naprawy.addItem("Wszystkie naprawy");
+                        try {
+                            pracownicyZBazy.get(pracownicy.getSelectedIndex()-1).getNaprawy().forEach( n ->naprawy.addItem(n.toString()));
+                        } catch (Exception exception) {
+                            exception.printStackTrace();
+                        }
+                    }
+                }
+                else // Wybrani wszyscy pracownicy
+                {
+                    if(naprawy.getSelectedIndex() !=0 ) // Wybrana jakaś naprawa
+                    {
+                        Object selected = naprawy.getSelectedItem();
+                        naprawy.removeAllItems();
+                        naprawy.addItem("Wszystkie naprawy");
+                        unikalneNaprawy.forEach(n->naprawy.addItem(n.toString()));
+                        naprawy.setSelectedItem(selected);
+                    }
+                    else //Wybrane wszystkie naprawy
+                    {
+                        naprawy.removeAllItems();
+                        naprawy.addItem("Wszystkie naprawy");
+                        unikalneNaprawy.forEach(n->naprawy.addItem(n.toString()));
+                    }
+                }
+
+                helper.setRunning(false);
+            }
+        });
+
+        naprawy.addActionListener(e -> {
+            if(!helper.isRunning())// Zabezpieczenie przeciwko zapętleniu
+            {
+                helper.setRunning(true);
+
+                if(naprawy.getSelectedIndex() != 0) // Wybrana jakaś naprawa
+                {
+                    if(pracownicy.getSelectedIndex() !=0 ) // Wybrany jakiś pracownik
+                    {
+                        Object selected = pracownicy.getSelectedItem();
+                        pracownicy.removeAllItems();
+                        pracownicy.addItem("Wszyscy pracownicy");
+                        naprawaZeStringa(unikalneNaprawy,naprawy).getOsoby().forEach(p->pracownicy.addItem(p.toString()));
+                        pracownicy.setSelectedItem(selected);
+                    }
+                    else //Wybrani wszyscy pracownicy
+                    {
+                        pracownicy.removeAllItems();
+                        pracownicy.addItem("Wszyscy pracownicy");
+                        naprawaZeStringa(unikalneNaprawy, naprawy).getOsoby().forEach(o -> pracownicy.addItem(o.toString()));
+                    }
+                }
+                else // Wybrane wszystkie naprawy
+                {
+                    if(pracownicy.getSelectedIndex() !=0 ) // Wybrany jakiś pracownik
+                    {
+                        Object selected = pracownicy.getSelectedItem();
+                        pracownicy.removeAllItems();
+                        pracownicy.addItem("Wszyscy pracownicy");
+                        pracownicyZBazy.forEach(p->pracownicy.addItem(p.toString()));
+                        pracownicy.setSelectedItem(selected);
+                    }
+                    else //Wybrani wszyscy pracownicy
+                    {
+                        pracownicy.removeAllItems();
+                        pracownicy.addItem("Wszyscy pracownicy");
+                        pracownicyZBazy.forEach(p->pracownicy.addItem(p.toString()));
+                    }
+                }
+
+                helper.setRunning(false);
+            }
+        });
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+        panel.add(pracownicyLabel);
+        panel.add(pracownicy);
+        panel.add(naprawyLabel);
+        panel.add(naprawy);
+        frame.setLayout(new GridLayout(4,1));
+//        frame.add(listaNapraw, BorderLayout.NORTH);
+        frame.add(naglowekLabel);
+        frame.add(panel);
+        frame.add(lista);
+//        frame.add(lista, BorderLayout.CENTER);
+    }
+
+    private static Naprawa naprawaZeStringa(Set<Naprawa> unikalneNaprawy, JComboBox<String> naprawy) {
+        Pattern p = Pattern.compile("Id naprawy: (\\d+),.*");
+        Matcher m = p.matcher(naprawy.getSelectedItem().toString());
+        if (m.matches()) {
+            return unikalneNaprawy.stream().filter(n -> n.getId() == Long.parseLong(m.group(1))).collect(Collectors.toList()).get(0);
+        }
+        return null;
+    }
+
+    private static void naprawaSzczegoly(List<Osoba> pracownicyZBazy, JLabel naglowekLabel, JComboBox<String> pracownicy, JComboBox<String> naprawy, DefaultListModel<String> listModel) {
+        naglowekLabel.setText("Szczegóły naprawy");
+        listModel.clear();
+        try {
+            Osoba pracownik = pracownicyZBazy.get(pracownicy.getSelectedIndex()-1);
+//TODO: sprawdzić indeksy
+            try{
+                listModel.addElement(pracownik.szczegolyPracownika()+"\n"+pracownik
+                        .getNaprawy().get(naprawy.getSelectedIndex()-1).szczegolyNaprawy());
+            }
+            catch (IndexOutOfBoundsException ignored){}
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static List<Osoba> pobierzPracownikowZBazy()
+    {
+        List<Osoba> pracownicy = new ArrayList<>();
+        StandardServiceRegistry registry = null;
+        SessionFactory sessionFactory = null;
+        try
+        {
+            registry = new StandardServiceRegistryBuilder()
+                    .configure()
+                    .build();
+            sessionFactory = new MetadataSources(registry)
+                    .buildMetadata()
+                    .buildSessionFactory();
+            Session session = sessionFactory.openSession();
+
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+
+            CriteriaQuery<Osoba> criteria = builder.createQuery( Osoba.class );
+            Root<Osoba> root = criteria.from( Osoba.class );
+            criteria.select( root );
+            pracownicy = session.createQuery( criteria )
+                    .getResultList()
+                    .stream().filter(o -> o.getTypyOsob()
+                            .contains(TypyOsoby.PRACOWNIK)).collect(Collectors.toList());
+
+            session.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+        finally {
+            if(sessionFactory !=null)
+            {
+                sessionFactory.close();
+            }
+        }
+        return pracownicy;
+    }
+
+    public static List<Naprawa> pobierzNaprawyZBazy()
+    {
+        List<Naprawa> naprawy = null;
+        StandardServiceRegistry registry = null;
+        SessionFactory sessionFactory = null;
+        try
+        {
+            registry = new StandardServiceRegistryBuilder()
+                    .configure()
+                    .build();
+            sessionFactory = new MetadataSources(registry)
+                    .buildMetadata()
+                    .buildSessionFactory();
+            Session session = sessionFactory.openSession();
+
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+
+            CriteriaQuery<Naprawa> criteria = builder.createQuery( Naprawa.class );
+            Root<Naprawa> root = criteria.from( Naprawa.class );
+            criteria.select( root );
+            naprawy = session.createQuery( criteria ).getResultList();
+
+            session.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+        finally {
+            if(sessionFactory !=null)
+            {
+                sessionFactory.close();
+            }
+        }
+        return naprawy;
     }
 
     public static void listaNapraw(JFrame frame)
@@ -187,101 +433,5 @@ public class Main
         frame.add(panel);
         frame.add(lista);
 //        frame.add(lista, BorderLayout.CENTER);
-    }
-
-    private static void naprawaSzczegoly(List<Osoba> pracownicyZBazy, JLabel naglowekLabel, JComboBox<String> pracownicy, JComboBox<String> naprawy, DefaultListModel<String> listModel) {
-        naglowekLabel.setText("Szczegóły naprawy");
-        listModel.clear();
-        try {
-            Osoba pracownik = pracownicyZBazy.get(pracownicy.getSelectedIndex()-1);
-//TODO: sprawdzić indeksy
-            try{
-                listModel.addElement(pracownik.szczegolyPracownika()+"\n"+pracownik
-                        .getNaprawy().get(naprawy.getSelectedIndex()-1).szczegolyNaprawy());
-            }
-            catch (IndexOutOfBoundsException ignored){}
-
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-    }
-
-    public static List<Osoba> pobierzPracownikowZBazy()
-    {
-        List<Osoba> pracownicy = new ArrayList<>();
-        StandardServiceRegistry registry = null;
-        SessionFactory sessionFactory = null;
-        try
-        {
-            registry = new StandardServiceRegistryBuilder()
-                    .configure()
-                    .build();
-            sessionFactory = new MetadataSources(registry)
-                    .buildMetadata()
-                    .buildSessionFactory();
-            Session session = sessionFactory.openSession();
-
-            CriteriaBuilder builder = session.getCriteriaBuilder();
-
-            CriteriaQuery<Osoba> criteria = builder.createQuery( Osoba.class );
-            Root<Osoba> root = criteria.from( Osoba.class );
-            criteria.select( root );
-            pracownicy = session.createQuery( criteria )
-                            .getResultList()
-                            .stream().filter(o -> o.getTypyOsob()
-                            .contains(TypyOsoby.PRACOWNIK)).collect(Collectors.toList());
-
-            session.close();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            StandardServiceRegistryBuilder.destroy(registry);
-        }
-        finally {
-            if(sessionFactory !=null)
-            {
-                sessionFactory.close();
-            }
-        }
-        return pracownicy;
-    }
-
-    public static List<Naprawa> pobierzNaprawyZBazy()
-    {
-        List<Naprawa> naprawy = null;
-        StandardServiceRegistry registry = null;
-        SessionFactory sessionFactory = null;
-        try
-        {
-            registry = new StandardServiceRegistryBuilder()
-                    .configure()
-                    .build();
-            sessionFactory = new MetadataSources(registry)
-                    .buildMetadata()
-                    .buildSessionFactory();
-            Session session = sessionFactory.openSession();
-
-            CriteriaBuilder builder = session.getCriteriaBuilder();
-
-            CriteriaQuery<Naprawa> criteria = builder.createQuery( Naprawa.class );
-            Root<Naprawa> root = criteria.from( Naprawa.class );
-            criteria.select( root );
-            naprawy = session.createQuery( criteria ).getResultList();
-
-            session.close();
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            StandardServiceRegistryBuilder.destroy(registry);
-        }
-        finally {
-            if(sessionFactory !=null)
-            {
-                sessionFactory.close();
-            }
-        }
-        return naprawy;
     }
 }
